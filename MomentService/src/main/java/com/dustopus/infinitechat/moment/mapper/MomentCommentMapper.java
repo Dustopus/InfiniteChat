18c052195bfbd3fad0ed6ce2f30943f52963edaa
Dustopus <dustopus@github.com>
2026-03-24 17:52:32 +0800

    feat: 添加朋友圈服务 MomentService（发布、点赞、评论、Redis缓存）

---
+++ MomentService/src/main/java/com/dustopus/infinitechat/moment/mapper/MomentCommentMapper.java
@@ -0,0 +1,9 @@
+package com.dustopus.infinitechat.moment.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dustopus.infinitechat.moment.model.MomentComment;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MomentCommentMapper extends BaseMapper<MomentComment> {
+}
---
+++ MomentService/src/main/java/com/dustopus/infinitechat/moment/mapper/MomentLikeMapper.java
@@ -0,0 +1,9 @@
+package com.dustopus.infinitechat.moment.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dustopus.infinitechat.moment.model.MomentLike;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MomentLikeMapper extends BaseMapper<MomentLike> {
+}
